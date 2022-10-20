a903968076b169cd9e0b7bc834b4542a920e5ba4
Griffon2012 <trifonov-ga@yandex.ru>
2022-10-21 00:25:59 +0300

    add task23

---
+++ task_23/Program.cs
@@ -0,0 +1,30 @@
+﻿void GetThirdPow(int number)
+{
+    int step = 1;
+    double? thirdPow = null;
+    string stringResult = "";
+
+    if (number < 1)
+    {
+        step = -1;
+    }
+
+    for (int i = 1; i != (number + step); i += step)
+    {
+        thirdPow = Math.Pow(i, 3);
+
+        stringResult = $"{thirdPow}";
+
+        if (i != number)
+        {
+            stringResult = stringResult + ", ";
+        }
+
+        Console.Write(stringResult);
+    }
+}
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+GetThirdPow(number);
